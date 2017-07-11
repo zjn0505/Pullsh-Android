029aca3d877deab7e0aedd097968e24f4ee28616
Eddykasp <maxkasperowski@gmail.com>
2017-07-11 13:07:53 +0800

    Add Dummy Query
    Make async task called by click listener to return dummy data.

--- chuck/src/main/java/com/android/neuandroid/MainActivity.java
+++ chuck/src/main/java/com/android/neuandroid/MainActivity.java
@@ -1,13 +1,54 @@
 package com.android.neuandroid;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView chuckText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        chuckText = (TextView) findViewById(R.id.tv_chuck);
+        chuckText.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                loadChuckJoke();
+            }
+        });
+    }
+
+    private void loadChuckJoke(){
+        new ChuckJokeTask().execute();
+    }
+
+    private class ChuckJokeTask extends AsyncTask<Void, Object, String>{
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            String result = "Joke.";
+            return result;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            chuckText.setText("Loading...");
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            chuckText.setText(s);
+        }
     }
 }
