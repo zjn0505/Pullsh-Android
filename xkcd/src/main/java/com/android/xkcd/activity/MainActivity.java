599a6660867f63130cdb9b5881d3b4b8a26ef3e7
Eddykasp <maxkasperowski@gmail.com>
2017-07-18 09:22:28 +0800

    Add image sharing option

--- xkcd/src/main/java/com/android/xkcd/activity/MainActivity.java
+++ xkcd/src/main/java/com/android/xkcd/activity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.android.xkcd.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Environment;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,6 +28,9 @@ import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -155,21 +163,35 @@ public class MainActivity extends AppCompatActivity {
                 launchAltDialog();
                 break;
             case R.id.goto_num_action:
-                NumberPickerDialog dialog = new NumberPickerDialog();
-                dialog.setTitle(getResources().getString(R.string.choose_specific_title));
-                dialog.setRange(1, mostRecent);
-                dialog.setListener(new NumberPickerDialog.INumberPickerDialogListener() {
-                    @Override
-                    public void onPositiveClick(int number) {
-                        loadXKCDpicById(number);
-                    }
+                if(currentPic != null){
+                    NumberPickerDialog dialog = new NumberPickerDialog();
+                    dialog.setTitle(getResources().getString(R.string.choose_specific_title));
+                    dialog.setRange(1, mostRecent);
+                    dialog.setListener(new NumberPickerDialog.INumberPickerDialogListener() {
+                        @Override
+                        public void onPositiveClick(int number) {
+                            loadXKCDpicById(number);
+                        }
 
-                    @Override
-                    public void onNegativeClick() {
+                        @Override
+                        public void onNegativeClick() {
 
-                    }
-                });
-                dialog.show(getSupportFragmentManager(), "DialogFragment");
+                        }
+                    });
+                    dialog.show(getSupportFragmentManager(), "DialogFragment");
+                    break;
+                } else {
+                    launchAltDialog();
+                }
+
+            case R.id.share_action:
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("image/png");
+                shareIntent.putExtra(Intent.EXTRA_STREAM, getLocalBitmapUri(imageView));
+                startActivity(Intent.createChooser(shareIntent, getResources()
+                        .getString(R.string.share_menu_entry)));
+                break;
+
         }
         return true;
     }
@@ -224,7 +246,24 @@ public class MainActivity extends AppCompatActivity {
 
         Glide.with(this)
                 .load(pic.img)
+                .asBitmap()
+                .listener(new RequestListener<String, Bitmap>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<Bitmap> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Bitmap resource, String model, Target<Bitmap> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        MainActivity.this.progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
+                .override(2000, 2000)
+                .into(imageView);
+                /*
                 .listener(new RequestListener<String, GlideDrawable>() {
+
                     @Override
                     public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
                         return false;
@@ -238,6 +277,7 @@ public class MainActivity extends AppCompatActivity {
                 })
                 .override(2000, 2000)
                 .into(imageView);
+                */
         creationDateText.setText(pic.day + "/" + pic.month + "/" + pic.year);
 
     }
@@ -288,5 +328,33 @@ public class MainActivity extends AppCompatActivity {
         if(lastImg != 0){
             loadXKCDpicById(lastImg);
         }
+    }
+
+    public Uri getLocalBitmapUri(ImageView imageView) {
+        // Extract Bitmap from ImageView drawable
+        Drawable drawable = imageView.getDrawable();
+        Bitmap bmp = null;
+        if (drawable instanceof BitmapDrawable){
+            bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        } else {
+            return null;
+        }
+        // Store image to default external storage directory
+        Uri bmpUri = null;
+        try {
+            File file = new File(getApplicationContext()
+                    .getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS),
+                    "share_image_" + System.currentTimeMillis() + "png");
+            //File file =  new File(Environment.getExternalStoragePublicDirectory(
+            //        Environment.DIRECTORY_DOWNLOADS), "share_image_" + System.currentTimeMillis() + ".png");
+            file.getParentFile().mkdirs();
+            FileOutputStream out = new FileOutputStream(file);
+            bmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+            out.close();
+            bmpUri = Uri.fromFile(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bmpUri;
     }
 }
