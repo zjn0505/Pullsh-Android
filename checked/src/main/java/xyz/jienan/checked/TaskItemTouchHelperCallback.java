aa3669a97e951f3190569cb6e0c72f9dc8be463f
Jienan Zhang <zjn0505@hotmail.com>
2017-10-24 18:41:47 +0800

    Finish swipe UI

--- checked/src/main/java/xyz/jienan/checked/TaskItemTouchHelperCallback.java
+++ checked/src/main/java/xyz/jienan/checked/TaskItemTouchHelperCallback.java
@@ -1,11 +1,16 @@
 package xyz.jienan.checked;
 
-import android.annotation.SuppressLint;
-import android.content.res.Resources;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
-import android.support.v4.view.ViewCompat;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.VectorDrawable;
+import android.support.graphics.drawable.VectorDrawableCompat;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.util.Log;
@@ -33,7 +38,21 @@ public class TaskItemTouchHelperCallback extends ItemTouchHelper.Callback {
 
     private boolean returnToPosition = false;
     private boolean hasSwipeLeftHapticSent = false;
+    private boolean hasSwipeRightPhaseOneHapticSent = false;
+    private boolean hasSwipeRightPhaseTwoHapticSent = false;
     private float swipeMaxPixel;
+
+    private Drawable drawableOngoing = null;
+    private Drawable drawablePending = null;
+    private Drawable drawableCompleted = null;
+    private Drawable drawableDeleted = null;
+    private Drawable[] drawables = {drawableOngoing, drawablePending, drawableCompleted};
+    private enum Status {
+        Ongoing,
+        Pending,
+        Completed
+    }
+
 
     public TaskItemTouchHelperCallback(TaskItemTouchHelperAdapter adapter) {
         mAdapter = adapter;
@@ -86,7 +105,6 @@ public class TaskItemTouchHelperCallback extends ItemTouchHelper.Callback {
 
     @Override
     public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-
         // init swipeMaxPixel to limit the swipe distance
         if (swipeMaxPixel == 0) {
             swipeMaxPixel =  viewHolder.itemView.getWidth() * SWIPE_MAX_RATIO;
@@ -99,6 +117,10 @@ public class TaskItemTouchHelperCallback extends ItemTouchHelper.Callback {
             if (tasks != null && indexInList >= 0) {
                 TaskEntity task = tasks.get(indexInList);
                 if (task != null) {
+                    float yText = viewHolder.itemView.getTop() + viewHolder.itemView.getHeight() / 4 * 3;
+                    float yIcon = viewHolder.itemView.getTop() + viewHolder.itemView.getHeight() / 4;
+                    int iconSize = viewHolder.itemView.getHeight() / 2;
+
                     if (dX < 0) {
                         ColorDrawable targetDrawable = new ColorDrawable(Color.RED);
                         if (isCurrentlyActive) {
@@ -112,12 +134,26 @@ public class TaskItemTouchHelperCallback extends ItemTouchHelper.Callback {
                                 viewHolder.itemView.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK);
                                 hasSwipeLeftHapticSent = true;
                             }
+                            if (drawableDeleted == null) {
+                                Context context = recyclerView.getContext();
+                                drawableDeleted = VectorDrawableCompat.create(context.getResources(), R.drawable.ic_delete, null);
+                            }
+
+                            drawableDeleted.setBounds(viewHolder.itemView.getRight() - iconSize - 20, (int)yIcon,
+                                    viewHolder.itemView.getRight() - 20, (int)yIcon + iconSize);
+                            drawableDeleted.draw(c);
+
                             super.onChildDraw(c, recyclerView, viewHolder, limitedDX, dY, actionState, isCurrentlyActive);
                         } else {
                             int width = viewHolder.itemView.getWidth();
-                            float ratio = (width+dX) / (width-swipeMaxPixel);
+                            float ratio;
+                            if (dX > -swipeMaxPixel) {
+                                ratio = Math.abs(dX)/(recyclerView.getWidth() * SWIPE_DELETE_THRESOLD);
+                                ratio = ratio * ratio;
+                            } else {
+                                ratio = (width+dX) / (width-swipeMaxPixel);
+                            }
                             ratio = ratio > 1 ? 1 : ratio < 0 ? 0 : ratio;
-                            Log.d(TAG, "onChildDraw: " + ratio);
                             targetDrawable.setAlpha((int)(ratio * 255));
                             targetDrawable.setBounds((int)(viewHolder.itemView.getRight()+ dX), viewHolder.itemView.getTop(), recyclerView.getRight(), viewHolder.itemView.getBottom());
                             targetDrawable.draw(c);
@@ -131,21 +167,76 @@ public class TaskItemTouchHelperCallback extends ItemTouchHelper.Callback {
                         if (statusList != null && statusList.size() > 0) {
 
                             String status = statusList.get(0);
+                            Status status1 = null, status2 = null;
                             if (status.equals("ongoing")) {
-                                colorArray = new int[]{0x00FFFFFF, 0xFFAAAAAA, 0xFFFFFF00};
+                                colorArray = new int[]{Color.TRANSPARENT, Color.GRAY, Color.YELLOW};
+                                status1 = Status.Completed;
+                                status2 = Status.Pending;
                             } else if (status.equals("pending")) {
-                                colorArray = new int[]{0x00FFFFFF, 0xFF00FF00, 0xFFAAAAAA};
+                                colorArray = new int[]{Color.TRANSPARENT, Color.GREEN, Color.GRAY};
+                                status1 = Status.Ongoing;
+                                status2 = Status.Completed;
                             } else if (status.equals("completed")) {
-                                colorArray = new int[]{0x00FFFFFF, 0xFFFFFF00, 0xFF00FF00};
+                                colorArray = new int[]{Color.TRANSPARENT, Color.YELLOW, Color.GREEN};
+                                status1 = Status.Pending;
+                                status2 = Status.Ongoing;
                             }
+                            float progress = dX / swipeMaxPixel;
+                            progress = progress > 1 ? 1 : progress;
+                            int color = ColorUtils.blendColor(colorArray, progress, 0, 0.5f,1);
+                            ColorDrawable targetDrawable = new ColorDrawable(color);
+                            targetDrawable.setBounds(0, viewHolder.itemView.getTop(), (int)(viewHolder.itemView.getLeft()+dX), viewHolder.itemView.getBottom());
+                            targetDrawable.draw(c);
+                            Paint p = new Paint();
+                            p.setTextSize(viewHolder.itemView.getHeight() / 2);
+                            p.setAntiAlias(true);
+                            p.setColor(Color.WHITE);
+                            p.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+
+                            if (drawableOngoing == null) {
+                                Context context = recyclerView.getContext();
+                                drawableOngoing = VectorDrawableCompat.create(context.getResources(), R.drawable.ic_ongoing, null);
+                                drawablePending = VectorDrawableCompat.create(context.getResources(), R.drawable.ic_pending, null);
+                                drawableCompleted = VectorDrawableCompat.create(context.getResources(), R.drawable.ic_check, null);
+                                drawables[0] = drawableOngoing;
+                                drawables[1] = drawablePending;
+                                drawables[2] = drawableCompleted;
+                            }
+
+                            float phaseOneSt = swipeMaxPixel * 0.3f, phaseOneEnd = swipeMaxPixel * 0.6f;
+                            if (Math.abs(dX) >= phaseOneSt && Math.abs(dX) <= phaseOneEnd && isCurrentlyActive) {
+                                if (!hasSwipeRightPhaseOneHapticSent) {
+                                    viewHolder.itemView.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK);
+                                    hasSwipeRightPhaseOneHapticSent = true;
+                                }
+                                float mid = (phaseOneEnd + phaseOneSt) / 2;
+                                int alpha = (int)((1 - Math.abs(dX - mid)/ (mid - phaseOneSt)) * 255);
+                                p.setAlpha(alpha);
+//                                c.drawText(status1.toString(), 20, y, p);
+                                Drawable drawable = drawables[status1.ordinal()];
+                                drawable.setAlpha(alpha);
+                                drawable.setBounds(20, (int)yIcon, 20 + iconSize, (int)yIcon + iconSize);
+                                drawable.draw(c);
+                            }
+
+                            float phaseTwoSt = 0.7f, phaseTwoEnd = 0.85f;
+                            if (Math.abs(dX) >= swipeMaxPixel*phaseTwoSt && isCurrentlyActive) {
+                                if (!hasSwipeRightPhaseTwoHapticSent) {
+                                    viewHolder.itemView.performHapticFeedback(HapticFeedbackConstants.CLOCK_TICK);
+                                    hasSwipeRightPhaseTwoHapticSent = true;
+                                }
+                                int alpha = (int)((dX - swipeMaxPixel * phaseTwoSt) / swipeMaxPixel/ (phaseTwoEnd-phaseTwoSt) * 255);
+                                alpha = dX >= swipeMaxPixel*phaseTwoEnd ? 255 : alpha;
+                                p.setAlpha(alpha);
+//                                c.drawText(status2.toString(), 20, y, p);
+                                Drawable drawable = drawables[status2.ordinal()];
+                                drawable.setAlpha(alpha);
+                                drawable.setBounds(20, (int)yIcon, 20 + iconSize, (int)yIcon + iconSize);
+                                drawable.draw(c);
+                            }
+                            super.onChildDraw(c, recyclerView, viewHolder, limitedDX, dY, actionState, isCurrentlyActive);
                         }
-                        float progress = dX / swipeMaxPixel;
-                        progress = progress > 1 ? 1 : progress;
-                        int color = ColorUtils.blendColor(colorArray, progress, 0, 0.5f,1);
-                        ColorDrawable targetDrawable = new ColorDrawable(color);
-                        targetDrawable.setBounds(0, viewHolder.itemView.getTop(), (int)(viewHolder.itemView.getLeft()+dX), viewHolder.itemView.getBottom());
-                        targetDrawable.draw(c);
-                        super.onChildDraw(c, recyclerView, viewHolder, limitedDX, dY, actionState, isCurrentlyActive);
+
                     }
                 }
             }
@@ -158,6 +249,8 @@ public class TaskItemTouchHelperCallback extends ItemTouchHelper.Callback {
     public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
         super.clearView(recyclerView, viewHolder);
         hasSwipeLeftHapticSent = false;
+        hasSwipeRightPhaseOneHapticSent = false;
+        hasSwipeRightPhaseTwoHapticSent = false;
     }
 
     private void setTouchListener(final Canvas c, final RecyclerView recyclerView, final RecyclerView.ViewHolder viewHolder, final float dX, final float dY, final int actionState, final boolean isCurrentlyActive) {
